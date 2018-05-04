fed363407c6abc12ffff6a865d0c199e6f795366
Manon Bourgoing <manon.bourgoing@edu.ece.fr>
2018-05-04 13:07:41 +0200

    TOUT INTÉGRÉ

---
+++ SALUT/Emploi.php
@@ -0,0 +1,217 @@
+<?php
+ session_start();
+ $bdd = new PDO('mysql:host=localhost;dbname=poolco;charset=utf8', 'root', 'root');
+ $requ = "SELECT * FROM offre";
+ $jack0 = $bdd->query($requ);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Emploi</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0px;      
+      border-radius: 0;
+      font-size:16px;
+      font-family: 'Poppins', serif;
+      font-weight: 400;
+      -webkit-font-smoothing: antialiased; 
+      -moz-osx-font-smoothing: grayscale; 
+      word-wrap: break-word; 
+    }
+
+    
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {
+      height: 700px;
+      padding-left: 80px;
+    }
+
+    .jj{
+      color:#FFA600;
+    }
+
+    
+    .photo
+    {
+      padding-top: 5px;
+    }
+
+    .jumbotron{
+      background-color: #FF007F;
+      color: white;
+
+      font-family: 'Poppins', serif;
+    }
+
+    .container{
+      font-family: 'Poppins', serif;
+    }
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 10px;
+    }
+    </style>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700%7CAllura" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <img class="photo" src="images/logo.png" size="100%" height="45" >      
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="./Accueil.php" title="">Accueil</a></li>
+        <li><a href="./Reseau.php">Mon réseau</a></li>
+        <li><a href="./Profil1.php" title="">Mon profil</a></li>
+        <li><a href="#">Notifications</a></li>
+        <li><a href="#">Messagerie</a></li>
+        <li class="active"><a href="./Emploi.php">Emploi</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Déconnexion</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<header class="w3-display-container w3-content w3-wide" id="home">
+  <img src="images/fond_emploi.jpg" width="1349" height="500">
+</header>
+
+<main role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Offres d'Emplois</h1>
+          <p>Basées sur des critères qui peuvent vous interesser</p>
+        </div>
+          <div class="container">
+          <br><button type="button" class="btn btn-warning btn-lg" href="#" onclick="ajouterOffre()">Ajouter une offre</button>
+<!-- Debut - Boite de dialogue -->
+     <!-- Fond de la boite de dialogue -->
+<div id="idFond" class="fond">
+&nbsp;
+</div>
+ 
+      <!-- Boite de dialogue -->
+<div id="monPrompt" class="conteneurPrompt">
+ 
+  <div class="contenuPrompt">
+    <!-- debut - elements de la boite de dialogue -->
+
+
+<form method="post" action="Profil1.php">
+    Titre de l'offre &nbsp;:
+    <input type="text" name="Titre" value="Votre texte" id="idText" size="20" maxlength="15" /><br /><br>
+    Prenom&nbsp;:
+    <input type="text" value="Votre texte" id="idText" size="20" maxlength="15" /><br />
+    Date de Naissance&nbsp;:
+    <input type="" value="Votre texte" id="idText" size="20" maxlength="15" /><br />
+    Email&nbsp;:
+    <input type="Email" value="Votre texte" id="idText" size="20" maxlength="15" /><br />
+ 
+    <div class="boutonsPrompt">
+      <br/>
+      <input type="button" value="Ok" id="idOk" class="ok" onclick="clicOk()" />
+      <input type="button" value="Annuler" id="idAnnuler" class="annuler" onclick="clicAnnuler()" />
+    </div>
+ 
+  </div>
+  </div>
+
+
+
+
+ 
+
+
+        </div>
+      </div>
+            <div class="container">
+        <!-- Example row of columns -->
+               <div class="row">
+                
+
+      <?php while($donnees = $jack0->fetch())
+      {
+          $_SESSION["titre_offre"] = $donnees['Titre'];
+          $_SESSION["des_offre"] = $donnees['Description'];
+          $_SESSION["nom_auteur_offre"] = $donnees['Nom_Auteur'];
+      ?>
+            <div class="col-md-4">
+            <h2><?php echo $_SESSION["titre_offre"];?></h2>
+            <h4 style="font-weight: bold;">publié par : <?php echo $_SESSION["nom_auteur_offre"];?></h4>
+            <p><?php echo $_SESSION["des_offre"];?></p>
+            <p><a class="jj" href="#" role="button">Postuler &raquo;</a></p></div>
+            <?php
+          }
+          ?>
+          </div>
+        </div>
+      </div> <br><!-- /container -->
+
+    </main>
+
+<footer class="container-fluid text-center">
+  <p>@PoolCorporation</p>
+</footer>
+
+
+<script>
+
+  clicAnnuler();
+
+
+ function fermeBoite()
+{
+  document.getElementById("monPrompt").style.display = "none";
+  document.getElementById("idFond").style.display = "none";
+}
+
+function ajouterOffre()
+{
+  document.getElementById("monPrompt").style.display = "block";
+  document.getElementById("idFond").style.display = "block";
+}
+ 
+ 
+/**************************************************************************/
+/** Actions realisees par l utilisateur si clic sur Ok ou Annuler *********/
+function clicOk()
+{
+  fermeBoite();
+ 
+  /** Autres actions a realiser si clic sur Ok **/
+  /** ... **/
+}
+ 
+ 
+function clicAnnuler()
+{
+  fermeBoite();
+ 
+  /** Autres actions a realiser si clic sur annuler **/
+  /** ... **/
+}
+  </script>
+
+</body>
+</html>
+
---
+++ SALUT/index.php
@@ -0,0 +1,256 @@
+<?php
+session_start();
+
+
+if(isset($_POST["submit1"])){
+
+$nom = isset($_POST['nom'])? $_POST['nom']:"";
+$prenom = isset($_POST['prenom'])? $_POST['prenom']:"";
+$email = isset($_POST['email'])? $_POST['email']:"";
+$mdp = isset($_POST['mdp'])? $_POST['mdp']:"";
+$naissance = isset($_POST['naissance'])? $_POST['naissance']:"";
+$sexe = isset($_POST['sexe'])? $_POST['sexe']:"";
+
+
+$bdd = new PDO('mysql:host=localhost;dbname=poolco;charset=utf8', 'root', 'root');
+$rqt2 = "SELECT MAX(id) FROM utilisateurs";
+$jack = $bdd->query($rqt2);
+$donnees = $jack->fetch();
+$nb = $donnees[0]+1;
+$_SESSION["id"] = $nb;
+$rqt = "INSERT INTO utilisateurs(id,nom,prenom,mail,naissance,mdp,sexe) VALUES ('$nb','$nom','$prenom','$email','$naissance','$mdp','$sexe')";
+if($email!=NULL)
+{
+ $bdd->query($rqt);
+}
+}
+
+if(isset($_POST["submit"])){
+
+	$email = isset($_POST['emailCo'])? $_POST['emailCo']:"";
+	$mdp = isset($_POST['mdpCo'])? $_POST['mdpCo']:"";
+
+ $_SESSION["mail"] = $email;
+
+	$bdd = new PDO('mysql:host=localhost;dbname=poolco;charset=utf8', 'root', 'root');
+	$reponse = "SELECT id, prenom, nom, mail, mdp, naissance, Photo, Statut FROM utilisateurs WHERE mail = '$email' AND mdp = '$mdp'";
+	$jack = $bdd->query($reponse);
+	$donnees = $jack->fetch();
+	$e = $donnees['id'];
+
+	$_SESSION["nom"] = $donnees['nom'];
+	$_SESSION["prenom"] = $donnees['prenom'];
+	$_SESSION["naissance"] = $donnees['naissance'];
+	$_SESSION["Photo"] = $donnees['Photo'];
+	$_SESSION["id"] = $donnees['id'];
+	$_SESSION["statut"] = $donnees['Statut'];
+	$_SESSION["abc"] = $donnees['id'];
+
+  	$pro = "SELECT * FROM profession WHERE id='$e'";
+ 	$jack2 = $bdd->query($pro);
+ 	$donnees2 = $jack2->fetch();
+ 	$_SESSION["apropos"] = $donnees2['apropos'];
+ 	$_SESSION["nom_poste1"] = $donnees2['nom_poste1'];
+ 	$_SESSION["entreprise1"] = $donnees2['entreprise1'];
+	$_SESSION["date_debut1"] = $donnees2['date_debut1'];
+ 	$_SESSION["date_fin1"] = $donnees2['date_fin1'];
+ 	$_SESSION["desc1"] = $donnees2['desc1'];
+ 	$_SESSION["nom_poste2"] = $donnees2['nom_poste2'];
+ 	$_SESSION["entreprise2"] = $donnees2['entreprise2'];
+ 	$_SESSION["date_debut2"] = $donnees2['date_debut2'];
+ 	$_SESSION["date_fin2"] = $donnees2['date_fin2'];
+ 	$_SESSION["desc2"] = $donnees2['desc2'];
+ 	$_SESSION["diplome1"] = $donnees2['diplome1'];
+ 	$_SESSION["universite1"] = $donnees2['universite1'];
+ 	$_SESSION["date_val1"] = $donnees2['date_val1'];
+ 	$_SESSION["descri1"] = $donnees2['descri1'];
+ 	$_SESSION["diplome2"] = $donnees2['diplome2'];
+ 	$_SESSION["universite2"] = $donnees2['universite2'];
+ 	$_SESSION["date_val2"] = $donnees2['date_val2'];
+ 	$_SESSION["descri2"] = $donnees2['descri2'];
+ 	$_SESSION["diplome3"] = $donnees2['diplome3'];
+ 	$_SESSION["universite3"] = $donnees2['universite3'];
+ 	$_SESSION["date_val3"] = $donnees2['date_val3'];
+ 	$_SESSION["descri3"] = $donnees2['descri3'];
+
+
+
+	if($donnees['mail']==$email && $donnees['mdp']==$mdp && $email!=NULL)
+	{
+		header('location:Profil1.php');
+
+	}
+
+
+}
+
+?>
+
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Pool Connexion</title>
+        <meta charset="utf-8">
+		<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+		<script src="js/cufon-yui.js" type="text/javascript"></script>
+		<script src="js/ChunkFive_400.font.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			Cufon.replace('h1',{ textShadow: '1px 1px #fff'});
+			Cufon.replace('h2',{ textShadow: '1px 1px #fff'});
+			Cufon.replace('h3',{ textShadow: '1px 1px #000'});
+			Cufon.replace('.back');
+		</script>
+    </head>
+    <body>
+		<div class="wrapper">
+			<div class="content">
+				<div id="form_wrapper" class="form_wrapper">
+					<form class="register" method="post" action="index.php">
+						<h3>S'inscrire</h3>
+						<div class="column">
+							<div>
+								<label>Nom</label>
+								<input type="text" name="nom"/>
+								<span class="error">Erreur</span>
+							</div>
+							<div>
+								<label>Prénom</label>
+								<input type="text" name="prenom"/>
+								<span class="error">Erreur</span>
+							</div>
+							<div>
+								<label>Adresse mail</label>
+								<input type="text" name="email"/>
+								<span class="error">Erreur</span>
+							</div>
+						</div>
+						<div class="column">
+							<div>
+								<label>Date de naissance</label>
+								<input type="date" name="naissance"/>
+								<span class="error">Erreur</span>
+							</div>
+							<div>
+								<label>Mot de passe</label>
+								<input type="password" name="mdp"/>
+								<span class="error">Erreur</span>
+							</div>
+							<div>
+								<label>Sexe</label><br>
+								<input type="radio" name="sexe" value="homme" id="homme">   Homme
+								&nbsp;
+								&nbsp;      
+								<input type="radio" name="sexe" value="femme" id="femme">   Femme<br>
+								<span class="error">Erreur</span>
+							</div>
+						</div>
+						<div class="bottom">
+							
+							<a href="index.html" rel="login" class="linkform">Vous avez déjà un compte ? Connectez-vous ici</a>
+							<div class="clear"></div>
+						</div>
+						<input type="submit" value="S'inscrire" name="submit1"/>
+					</form>
+
+					<form class="login active" method="post" action="index.php">
+						<h3>Connection</h3>
+						<div>
+							<label>Adresse mail</label>
+							<input type="text" name="emailCo"/>
+							<span class="error">Erreur</span>
+						</div>
+						<div>
+							<label>Mot de passe</label>
+							<input type="password" name="mdpCo"/>
+							<span class="error">Erreur</span>
+						</div>
+						<div class="bottom">
+							<input type="submit" value="Se connecter" name="submit" >
+							<a href="register.html" rel="register" class="linkform">Vous n'avez pas encore de compte ? Inscrivez-vous ici</a>
+							<div class="clear"></div>
+						</div>
+					</form>
+					
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+		
+
+		<!-- The JavaScript -->
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript">
+			$(function() {
+					//the form wrapper (includes all forms)
+				var $form_wrapper	= $('#form_wrapper'),
+					//the current form is the one with class active
+					$currentForm	= $form_wrapper.children('form.active'),
+					//the change form links
+					$linkform		= $form_wrapper.find('.linkform');
+						
+				//get width and height of each form and store them for later						
+				$form_wrapper.children('form').each(function(i){
+					var $theForm	= $(this);
+					//solve the inline display none problem when using fadeIn fadeOut
+					if(!$theForm.hasClass('active'))
+						$theForm.hide();
+					$theForm.data({
+						width	: $theForm.width(),
+						height	: $theForm.height()
+					});
+				});
+				
+				//set width and height of wrapper (same of current form)
+				setWrapperWidth();
+				
+				/*
+				clicking a link (change form event) in the form
+				makes the current form hide.
+				The wrapper animates its width and height to the 
+				width and height of the new current form.
+				After the animation, the new form is shown
+				*/
+				$linkform.bind('click',function(e){
+					var $link	= $(this);
+					var target	= $link.attr('rel');
+					$currentForm.fadeOut(400,function(){
+						//remove class active from current form
+						$currentForm.removeClass('active');
+						//new current form
+						$currentForm= $form_wrapper.children('form.'+target);
+						//animate the wrapper
+						$form_wrapper.stop()
+									 .animate({
+										width	: $currentForm.data('width') + 'px',
+										height	: $currentForm.data('height') + 'px'
+									 },500,function(){
+										//new form gets class active
+										$currentForm.addClass('active');
+										//show the new form
+										$currentForm.fadeIn(400);
+									 });
+					});
+					e.preventDefault();
+				});
+				
+				function setWrapperWidth(){
+					$form_wrapper.css({
+						width	: $currentForm.data('width') + 'px',
+						height	: $currentForm.data('height') + 'px'
+					});
+				}
+								
+				$form_wrapper.find('input[id="connect"]').click(function(e)
+				{
+						alert("dd");
+				});
+
+				$form_wrapper.find('input[id="inscription"]').click(function(e)
+				{
+						alert("gjh");
+				});	
+			});
+        </script>
+    </body>
+</html>
